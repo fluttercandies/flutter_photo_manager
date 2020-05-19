d6c04cf18378057928be96af3b3b250c1764d857
Caijinglong <cjl_spy@163.com>
2020-05-19 16:37:08 +0800

    Add glide kapt config for example.

---
+++ example/android/app/src/main/kotlin/top/kikt/imagescannerexample/ExampleAppGlideModule.java
@@ -0,0 +1,9 @@
+package top.kikt.imagescannerexample;
+/// create 2020/5/19 by cai
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class ExampleAppGlideModule extends AppGlideModule {
+}
