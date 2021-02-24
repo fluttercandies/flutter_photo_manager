9a747e3c9964ec63a0e35187f03414dcc0c416e9
alexv525 <github@alexv525.com>
2021-02-24 13:55:08 +0800

    ♻️ Android demo v1 -> v2

--- example/android/app/src/main/kotlin/top/kikt/imagescannerexample/MainActivity.kt
+++ example/android/app/src/main/kotlin/top/kikt/imagescannerexample/MainActivity.kt
@@ -2,14 +2,11 @@ package top.kikt.imagescannerexample
 
 import android.os.Bundle
 
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity() : FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        GeneratedPluginRegistrant.registerWith(this)
-
         CrashHandler.initHandler(this)
     }
 }
