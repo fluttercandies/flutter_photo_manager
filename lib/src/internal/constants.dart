0f35b33916a3acc83817bfd36a4779f2f5dd29af
Alex Li <github@alexv525.com>
2021-10-19 13:55:01 +0800

    ✨ Add constant channel name

---
+++ lib/src/internal/constants.dart
@@ -0,0 +1,9 @@
+///
+/// [Author] Alex (https://github.com/AlexV525)
+/// [Date] 2021/10/19 13:50
+///
+class PMConstants {
+  const PMConstants._();
+
+  static const String channelPrefix = 'com.fluttercandies/photo_manager';
+}
