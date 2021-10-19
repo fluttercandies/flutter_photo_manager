edbcec363903665d567d5c9dbe93b6d7104ebe8e
Alex Li <github@alexv525.com>
2021-10-19 13:34:50 +0800

    ♻️ Rename org to `com.fluttercandies` on Android

---
+++ android/src/main/kotlin/com/fluttercandies/photo_manager/core/entity/GalleryEntity.kt
@@ -0,0 +1,11 @@
+package com.fluttercandies.photo_manager.core.entity
+
+/// create 2019-09-05 by cai
+data class GalleryEntity(
+    val id: String,
+    val name: String,
+    var length: Int,
+    val typeInt: Int,
+    var isAll: Boolean = false,
+    var modifiedDate: Long? = null
+)
