7ffcfbc20d41acdfd81c28e3bd5dc5964e7c1424
Bruno D'Luka <45696119+bdlukaa@users.noreply.github.com>
2021-12-08 13:30:38 -0300

    Add NotifyChangeInfo

--- lib/src/notify.dart
+++ lib/src/notify.dart
@@ -1,5 +1,7 @@
 part of '../photo_manager.dart';
 
+typedef NotifyChangeInfoCallback = void Function(NotifyChangeInfo info);
+
 /// manage photo changes
 ///
 /// 当相册发生变化时, 通知
@@ -13,15 +15,17 @@ class _NotifyManager {
   Stream<bool> get notifyStream => _controller.stream;
 
   /// callbacks
-  var notifyCallback = <ValueChanged<MethodCall>>[];
+  final _notifyCallbacks = <NotifyChangeInfoCallback>[];
 
   /// add callback
-  void addCallback(ValueChanged<MethodCall> callback) =>
-      notifyCallback.add(callback);
+  void addCallback(NotifyChangeInfoCallback callback) {
+    _notifyCallbacks.add(callback);
+  }
 
   /// remove callback
-  void removeCallback(ValueChanged<MethodCall> callback) =>
-      notifyCallback.remove(callback);
+  void removeCallback(NotifyChangeInfoCallback callback) {
+    _notifyCallbacks.remove(callback);
+  }
 
   /// start handle notify
   void startHandleNotify() {
@@ -39,15 +43,73 @@ class _NotifyManager {
 
   Future<dynamic> _notify(MethodCall call) async {
     if (call.method == "change") {
-      print(call.arguments);
-      _onChange(call);
+      final info =
+          NotifyChangeInfo.fromJson(call.arguments as Map<String, dynamic>);
+      debugPrint(info.toString());
+      _onChange(info);
     } else if (call.method == "setAndroidQExperimental") {
       // PhotoManager.androidQExperimental = call.arguments["open"];
     }
     return 1;
   }
 
-  Future<dynamic> _onChange(MethodCall methodCall) async {
-    notifyCallback.toList().forEach((callback) => callback.call(methodCall));
+  void _onChange(NotifyChangeInfo info) {
+    _notifyCallbacks.toList().forEach((callback) => callback.call(info));
   }
 }
+
+class NotifyChangeInfo {
+  final String platform;
+  final Uri uri;
+  final NotifyChangeType changeType;
+  final AssetType mediaType;
+
+  const NotifyChangeInfo.raw({
+    required this.platform,
+    required this.uri,
+    required this.changeType,
+    required this.mediaType,
+  });
+
+  factory NotifyChangeInfo.fromJson(Map<String, dynamic> map) {
+    return NotifyChangeInfo.raw(
+      platform: map['platform'],
+      uri: Uri.parse(map['uri']),
+      changeType: _parseNotifyChangeType(map['type']),
+      mediaType: _parseTypeFromId(map['mediaType']),
+    );
+  }
+
+  static AssetType _parseTypeFromId(int type) {
+    switch (type) {
+      case 1:
+        return AssetType.image;
+      case 2:
+        return AssetType.video;
+      case 3:
+        return AssetType.audio;
+      default:
+        return AssetType.other;
+    }
+  }
+
+  static NotifyChangeType _parseNotifyChangeType(String type) {
+    switch (type) {
+      case 'delete':
+        return NotifyChangeType.delete;
+      case 'insert':
+        return NotifyChangeType.insert;
+      case 'update':
+        return NotifyChangeType.update;
+      default:
+        throw UnimplementedError('$type is not present in NotifyChangeType');
+    }
+  }
+
+  @override
+  String toString() {
+    return 'NotifyChangeInfo(platform: $platform, uri: $uri, changeType: $changeType, mediaType: $mediaType)';
+  }
+}
+
+enum NotifyChangeType { delete, insert, update }
