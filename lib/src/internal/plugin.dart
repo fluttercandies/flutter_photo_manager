2c4b15caf3587c8570acfd8778916ae6236cc8aa
Alex Li <github@alexv525.com>
2022-05-23 11:16:30 +0800

    ✏️ Correct `PermissionRequestOption` with a class type alias (#770)

--- lib/src/internal/plugin.dart
+++ lib/src/internal/plugin.dart
@@ -67,7 +67,7 @@ class PhotoManagerPlugin with BasePlugin, IosPlugin, AndroidPlugin {
   }
 
   Future<PermissionState> requestPermissionExtend(
-    PermisstionRequestOption requestOption,
+    PermissionRequestOption requestOption,
   ) async {
     final int result = await _channel.invokeMethod<int>(
       PMConstants.mRequestPermissionExtend,
