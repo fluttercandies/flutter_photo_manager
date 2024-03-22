07b614b25ad5dc374a491333288e606749c88506
Ic3Sandy <36376937+Ic3Sandy@users.noreply.github.com>
2024-03-22 15:53:19 +0700

    Add field playbackStyle to DarwinColumns for imporve filter IOS (#1062)

---
+++ example/lib/page/custom_filter/custom_filter_sql_gif_image.dart
@@ -0,0 +1,114 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:photo_manager/photo_manager.dart';
+
+import 'order_by_action.dart';
+
+class CustomFilterSqlGIFImage extends StatefulWidget {
+  const CustomFilterSqlGIFImage({
+    super.key,
+    required this.builder,
+  });
+
+  final Widget Function(BuildContext context, CustomFilter filter) builder;
+
+  @override
+  State<CustomFilterSqlGIFImage> createState() => _CustomFilterSqlGIFImageState();
+}
+
+class _CustomFilterSqlGIFImageState extends State<CustomFilterSqlGIFImage> {
+  final TextEditingController _whereController = TextEditingController();
+  final List<OrderByItem> _orderBy = [
+    OrderByItem.named(
+      column: CustomColumns.base.createDate,
+      isAsc: false,
+    ),
+  ];
+
+  late CustomFilter filter;
+
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) {
+      const columns = CustomColumns.android;
+      _whereController.text = "${columns.mimeType} == 'image/gif'";
+    } else if (Platform.isIOS || Platform.isMacOS) {
+      const columns = CustomColumns.darwin;
+      _whereController.text = '${columns.playbackStyle} == 2';
+    }
+
+    filter = createCustomFilter();
+  }
+
+  @override
+  void dispose() {
+    _whereController.dispose();
+    super.dispose();
+  }
+
+  void refresh() {
+    setState(() {
+      filter = createCustomFilter();
+    });
+  }
+
+  CustomFilter createCustomFilter() {
+    final filter = CustomFilter.sql(
+      where: _whereController.text,
+      orderBy: _orderBy,
+    );
+    return filter;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Custom Filter'),
+        actions: [
+          OrderByAction(
+            items: _orderBy,
+            onChanged: (List<OrderByItem> value) {
+              _orderBy.clear();
+              _orderBy.addAll(value);
+              refresh();
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _whereController,
+            decoration: const InputDecoration(
+              labelText: 'Where',
+            ),
+            onSubmitted: (value) {
+              refresh();
+            },
+            onEditingComplete: () {
+              refresh();
+            },
+          ),
+          ListTile(
+            title: Text(
+                'Order By: \n${_orderBy.map((e) => e.toString()).join('\n')}'),
+            subtitle: const Text('Click to edit'),
+            onTap: () {
+              changeOrderBy(context, _orderBy, (List<OrderByItem> value) {
+                _orderBy.clear();
+                _orderBy.addAll(value);
+                refresh();
+              });
+            },
+          ),
+          Expanded(
+            child: widget.builder(context, filter),
+          ),
+        ],
+      ),
+    );
+  }
+}
