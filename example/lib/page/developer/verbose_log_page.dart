8eea00255638dd7a941a7333d9617150df638408
Caijinglong <cjl_spy@163.com>
2024-08-06 17:34:13 +0800

    dev: change verbose log page
    Signed-off-by: Caijinglong <cjl_spy@163.com>

--- example/lib/page/developer/verbose_log_page.dart
+++ example/lib/page/developer/verbose_log_page.dart
@@ -12,8 +12,29 @@ class VerboseLogPage extends StatefulWidget {
   State<VerboseLogPage> createState() => _VerboseLogPageState();
 }
 
+class _LogUnit {
+  _LogUnit({required this.log});
+
+  final String log;
+
+  bool get isResultLog => log.contains('- result -');
+
+  bool get isInvokeLog => log.contains('- invoke -');
+
+  int? get swTime {
+    final reg = RegExp(r'Time: (\d+)ms');
+    final match = reg.firstMatch(log);
+    if (match != null) {
+      return int.parse(match.group(1)!);
+    }
+    return null;
+  }
+}
+
 class _VerboseLogPageState extends State<VerboseLogPage> {
-  final List<String> _logList = <String>[];
+  final List<_LogUnit> logList = <_LogUnit>[];
+
+  final List<String> originList = <String>[];
 
   @override
   void initState() {
@@ -21,19 +42,42 @@ class _VerboseLogPageState extends State<VerboseLogPage> {
     _loadLog();
   }
 
+  List<_LogUnit> decodeUnit(String text) {
+    final List<_LogUnit> list = <_LogUnit>[];
+    final List<String> items = text.split('===');
+    for (final String item in items) {
+      list.add(_LogUnit(log: item.trim()));
+    }
+    return list;
+  }
+
   Future<void> _loadLog() async {
     final logFilePath = await PMVerboseLogUtil.shared.getLogFilePath();
     final file = File(logFilePath);
     if (!file.existsSync()) {
-      _logList.clear();
+      logList.clear();
       setState(() {});
       return;
     }
-    final lines = file.readAsLinesSync();
-    _logList.clear();
+    final content = file.readAsStringSync();
+    logList.clear();
     setState(() {
-      _logList.addAll(lines);
+      logList.addAll(decodeUnit(content));
     });
+  }
+
+  Widget content(String text) {
+    final lines = text.split('\n');
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        for (final line in lines)
+          Text(
+            line,
+            maxLines: 1,
+          ),
+      ],
+    );
   }
 
   @override
@@ -64,18 +108,43 @@ class _VerboseLogPageState extends State<VerboseLogPage> {
           IconButton(
             icon: const Icon(Icons.copy),
             onPressed: () {
-              final log = _logList.join('\n');
-              Clipboard.setData(ClipboardData(text: log));
+              final content = logList.map((e) => e.log).join('\n');
+              Clipboard.setData(ClipboardData(text: content));
             },
           ),
         ],
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          return Text(_logList[index], maxLines: 1);
-        },
-        itemCount: _logList.length,
+      body: Column(
+        children: [
+          _buildFilter(),
+          Expanded(
+            child: ListView.separated(
+              itemBuilder: (context, index) {
+                final item = logList[index];
+                return ListTile(
+                  title: content(item.log),
+                  tileColor: item.isResultLog
+                      ? Colors.green.withOpacity(0.1)
+                      : item.isInvokeLog
+                          ? Colors.blue.withOpacity(0.1)
+                          : null,
+                  subtitle: item.swTime != null
+                      ? Text('Time: ${item.swTime}ms')
+                      : null,
+                );
+              },
+              itemCount: logList.length,
+              separatorBuilder: (context, index) => const Divider(
+                height: 2,
+              ),
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  Widget _buildFilter() {
+    return Container();
   }
 }
