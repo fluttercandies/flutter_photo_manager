3da6fade4e4f24e227886e930eefce5845a28db6
Alex Li <github@alexv525.com>
2024-12-28 17:31:33 +0800

    🎨 Improve `PMCancelToken`

--- lib/src/types/cancel_token.dart
+++ lib/src/types/cancel_token.dart
@@ -1,26 +1,25 @@
+import 'package:flutter/foundation.dart';
+
 import '../internal/plugin.dart';
 
 /// The cancel token is used to cancel the request.
 class PMCancelToken {
-  PMCancelToken({this.debugLabel}) : index = getIndex();
+  PMCancelToken({this.debugLabel}) : index = _index++;
 
   static int _index = 0;
 
-  static int getIndex() {
-    final res = _index;
-    _index++;
-    return res;
-  }
-
-  final String? debugLabel;
   final int index;
+  final String? debugLabel;
 
-  /// The key of cancel token, usually to use by [PhotoManagerPlugin.cancelRequest].
+  /// The key of cancel token, usually to use by
+  /// [PhotoManagerPlugin.cancelRequest].
   /// User don't need to use this.
+  @nonVirtual
   String get key => _index.toString();
 
   /// Cancel the request.
-  Future<void> cancelRequest() async {
-    await plugin.cancelRequest(this);
-  }
+  Future<void> cancelRequest() => plugin.cancelRequest(this);
+
+  @override
+  String toString() => 'PMCancelToken($key)';
 }
